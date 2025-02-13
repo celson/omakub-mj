8ed20b5a1f1eaf9225fbb2f1e16909391605e7fd
Celson Simon A. Vieira <celson.simon@gmail.com>
2025-02-13 03:04:16 -0300

    Update app-lazydocker.sh

--- install/terminal/app-lazydocker.sh
+++ install/terminal/app-lazydocker.sh
@@ -1,6 +1,6 @@
-# Arch under WSL2 already comes with fakeroot-tcp
-if ![ pacman -Q fakeroot-tcp ] &>/dev/null; then
-  sudo pacman -S --needed fakeroot
-fi
+# # Arch under WSL2 already comes with fakeroot-tcp
+# if ![ pacman -Q fakeroot-tcp ] &>/dev/null; then
+#   sudo pacman -S --needed fakeroot
+# fi
 
-yay -S --needed lazydocker-bin --noconfirm
+# yay -S --needed lazydocker-bin --noconfirm
