7b11c2923a606fe4292a137682ad5bf6fea0d613
Celson Simon A. Vieira <celson.simon@gmail.com>
2025-04-26 14:42:26 -0300

    feat: change optional apps

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,11 +1,11 @@
 OPTIONAL_APPS=("Bitwarden" "Spotify" "Zoom" "Discord" "Dropbox")
-DEFAULT_OPTIONAL_APPS='Bitwarden,Spotify'
+DEFAULT_OPTIONAL_APPS=''
 export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
-SELECTED_LANGUAGES="Ruby on Rails","Node.js"
+SELECTED_LANGUAGES=""
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
 AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
-SELECTED_DBS="PostgreSQL,Redis"
+SELECTED_DBS=""
 export OMAKUB_FIRST_RUN_DBS=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --selected "$SELECTED_DBS" --height 5 --header "Select databases (runs in Docker)")
