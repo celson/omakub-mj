ce43d1a5835285bd5d0efcd9407e35f67046d79d
Celson Simon A. Vieira <celson.simon@gmail.com>
2025-04-26 13:35:08 -0300

    feat: add jetbrains font

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -1,1 +1,1 @@
-yay -S --needed ttf-cascadia-mono-nerd ttf-ia-writer ttf-firacode-nerd ttf-hack ttf-meslo-nerd --noconfirm
+yay -S --needed ttf-cascadia-mono-nerd ttf-ia-writer ttf-firacode-nerd ttf-hack ttf-meslo-nerd ttf-jetbrains-mono-nerd --noconfirm
