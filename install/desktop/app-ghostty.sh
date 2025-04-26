fbf750c6a08746501d27a5440043d423b8f10ad7
Celson Simon A. Vieira <celson.simon@gmail.com>
2025-04-26 14:51:40 -0300

    feat: create config dirs of ghostty

--- install/desktop/app-ghostty.sh
+++ install/desktop/app-ghostty.sh
@@ -1,5 +1,6 @@
 # Ghostty is a fast, feature-rich, and cross-platform terminal emulator that uses platform-native UI and GPU acceleration.
 yay -S --needed ghostty --noconfirm
 
+mkdir -p ~/.config/ghostty ~/.config/ghostty/themes
 cp ~/.local/share/omakub/configs/ghostty.conf ~/.config/ghostty/config
 cp ~/.local/share/omakub/themes/catppuccin/ghostty.conf ~/.config/ghostty/themes/catppuccin-mocha.conf
